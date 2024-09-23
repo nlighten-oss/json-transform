d6b428c358315c4b26a3a4d943ce3878eb15c129
Eli Sherer <eli.sherer@gmail.com>
2024-09-23 12:19:02 +0300

    remove dummy test

--- javascript/json-transform/src/__tests__/DocumentContext.test.ts
+++ javascript/json-transform/src/__tests__/DocumentContext.test.ts
@@ -1,16 +1,7 @@
 import { describe, expect, test } from "vitest";
 import DocumentContext from "../DocumentContext";
-import TextEncoding from "../functions/common/TextEncoding";
 
 describe("DocumentContext", () => {
-  test("asda", () => {
-    const X =
-      "hello%FE%FF%00%2Bunicode%FE%FF%00%2B%D8%3D%DE%00%D8%3D%DC%68%20%0D%D8%3D%DC%69%20%0D%D8%3D%DC%67%20%0D%D8%3D%DC%66%D8%3E%DE%AC";
-    const u = new URLSearchParams("?x=" + X);
-    expect(u.get("x")).toBe("hello+unicode+😀👨‍👩‍👧‍👦🪬");
-    ///    expect(TextEncoding.decode(u8a, "UTF-16")).toBe("hello+unicode+😀👨‍👩‍👧‍👦🪬");
-  });
-
   test("Query root", () => {
     const obj = { x: 1 };
     const dc = new DocumentContext(obj);
