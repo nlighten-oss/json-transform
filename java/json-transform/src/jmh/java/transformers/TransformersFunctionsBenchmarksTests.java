c2abbe63969f6487d8f42ca2bbd3790297aaf309
Eli Sherer <eli.sherer@gmail.com>
2025-02-14 09:25:39 +0200

    fix jmh to be per adapter type

--- java/json-transform/src/jmh/java/transformers/TransformersFunctionsBenchmarksTests.java
+++ java/json-transform/src/jmh/java/transformers/TransformersFunctionsBenchmarksTests.java
@@ -1,6 +1,7 @@
 package transformers;
 
 import co.nlighten.jsontransform.JsonTransformerTest;
+import co.nlighten.jsontransform.adapters.JsonAdapter;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
@@ -13,10 +14,30 @@ import java.util.concurrent.TimeUnit;
 @State(value = Scope.Benchmark)
 public class TransformersFunctionsBenchmarksTests {
 
+    @Param({"gson", "jackson", "pojo", "jsonOrg", "jsonSmart"})
+    public String adapterType;
+
+    public JsonAdapter<?, ?, ?> getAdapterByType(String adapterType) {
+        switch (adapterType) {
+            case "gson":
+                return new co.nlighten.jsontransform.adapters.gson.GsonJsonAdapter();
+            case "jackson":
+                return new co.nlighten.jsontransform.adapters.jackson.JacksonJsonAdapter();
+            case "pojo":
+                return new co.nlighten.jsontransform.adapters.pojo.PojoJsonAdapter();
+            case "jsonOrg":
+                return new co.nlighten.jsontransform.adapters.jsonorg.JsonOrgJsonAdapter();
+            case "jsonSmart":
+                return new co.nlighten.jsontransform.adapters.jsonsmart.JsonSmartJsonAdapter();
+            default:
+                throw new IllegalArgumentException("Invalid adapter type: " + adapterType);
+        }
+    }
+
     @Benchmark
     @Measurement(iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-    public void measureInputExtractorSpreadArray2(){
+    public void measureInputExtractorSpreadArray2() {
         var test = new JsonTransformerTest();
-        test.testInputExtractorSpreadArray2();
+        test.testInputExtractorSpreadArray2(getAdapterByType(adapterType));
     }
 }
